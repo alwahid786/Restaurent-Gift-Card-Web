6ad2659b86cb8b1dd2b5f8d4459ac41880ddb176
Wahid Ahmad <wahid.a@kodextech.com>
2022-10-18 12:17:45 +0500

    Update GiftController.php

--- app/Http/Controllers/GiftController.php
+++ app/Http/Controllers/GiftController.php
@@ -118,6 +118,7 @@ class GiftController extends Controller
         // Add amount to restaurent balance
         $restaurent = User::find($loginUserId);
         $total_balance = Restaurent::where('id', $restaurent->restaurent_id)->get(['total_balance','released_balance']);
+        dd($total_balance);
         $newBalance = $total_balance['total_balance'] + $usedAmount;
         $pending_balance = $total_balance['total_balance'] - $total_balance['released_balance'];
         $saveData = Restaurent::where('id', $restaurent->restaurent_id)->update(['total_balance' => $newBalance, 'pending_balance' => $pending_balance]);
